6fa572c1329aaeee3630cbcccbec9959639af97d
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2024-06-26 12:46:14 +0530

    buildings have colour

--- building.js
+++ building.js
@@ -1,13 +1,26 @@
 let scene = document.getElementById("scene");
 
-    for (let x = 0; x < 10; x++) {
-        for (let z = 0; z < 10; z++) {
+    for (let x = 0; x < 20; x++) {
+        for (let z = 0; z < 20; z++) {
             let building = document.createElement("a-box");
-            building.setAttribute("position", `${x} 0 ${z}`);
-            building.setAttribute("width", 1);
-            building.setAttribute("height", Math.random() * 9 + 1);
-            building.setAttribute("color", "#CCC");  // Adding depth attribute
+            let height = Math.random() * 27 + 3;
+            building.setAttribute("height", height);
+            building.setAttribute("position", `${x * 4 - (x%4) - 36} ${height/2 - 40} ${z*4 - (z%4) -36}`);
+            building.setAttribute("width", 3);
+            building.setAttribute("depth", 3);
+            building.setAttribute("src", "window.png");
+            building.setAttribute("roughness", 0)
+            building.setAttribute("repeat",`10 ${height/16}`);
+            let colour = generateRandomGrey();
+            building.setAttribute("color", colour);
             scene.appendChild(building);
             console.log(building);
         }
     }
+
+function generateRandomGrey(){
+    let base = Math.floor(Math.random() * 255);
+    let hex = base.toString(16).padStart(2,"0");
+    let colour = '#' + hex + hex + hex;
+    return colour
+}
